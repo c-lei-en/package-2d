c78a9e27636dc337491e5997dfcf73c1ff2b52e2
c-lei-cn <1762928628@qq.com>
2020-06-29 09:28:04 +0800

    地图初始化

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,15 @@ module.exports = {
   // 将构建好的文件输出到哪里
   outputDir: "dist",
 
+  //静态资源 (js、css、img、fonts) 目录 (相对于 outputDir 的)
+  assetsDir: "assets",
+
+  //指定生成的 index.html 的输出路径 (相对于 outputDir)
+  indexPath: "index.html",
+
+  //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
+  filenameHashing: true,
+
   // 是否在保存的时候使用 `eslint-loader` 进行检查。
   // 有效的值：`ture` | `false` | `"error"`
   // 当设置为 `"error"` 时，检查出的错误会触发编译失败。
@@ -19,56 +28,14 @@ module.exports = {
   // 使用带有浏览器内编译器的完整构建版本
   // 查阅 https://cn.vuejs.org/v2/guide/installation.html#运行时-编译器-vs-只包含运行时
   // compiler: false,
-  runtimeCompiler: true, //关键点在这
-  // 调整内部的 webpack 配置。
-  // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/webpack.md
-  chainWebpack: () => {},
-  configureWebpack: () => {},
-
-  // vue-loader 选项
-  // 查阅 https://vue-loader.vuejs.org/zh-cn/options.html
-  // vueLoader: {},
+  runtimeCompiler: false, //关键点在这
 
   // 是否为生产环境构建生成 source map？
-  productionSourceMap: true,
-
-  // CSS 相关选项
-  css: {
-    // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
-    extract: true,
-
-    // 是否开启 CSS source map？
-    sourceMap: false,
-
-    // 为预处理器的 loader 传递自定义选项。比如传递给
-    // sass-loader 时，使用 `{ sass: { ... } }`。
-    loaderOptions: {},
-
-    // 为所有的 CSS 及其预处理文件开启 CSS Modules。
-    // 这个选项不会影响 `*.vue` 文件。
-    modules: false
-  },
-
-  // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
-  // 在多核机器下会默认开启。
-  parallel: require("os").cpus().length > 1,
-
-  // 是否使用 `autoDLLPlugin` 分割供应的包？
-  // 也可以是一个在 DLL 包中引入的依赖的显性的数组。
-  // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/cli-service.md#dll-模式
-  // dll: false,
-
-  // PWA 插件的选项。
-  // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli-plugin-pwa/README.md
-  pwa: {},
+  productionSourceMap: false,
 
   // 配置 webpack-dev-server 行为。
   devServer: {
-    open: process.platform === "darwin",
-    host: "0.0.0.0",
     port: 3000,
-    https: false,
-    hotOnly: false,
     // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/cli-service.md#配置代理
     proxy: null // string | Object
     // before: app => {
@@ -76,8 +43,39 @@ module.exports = {
     // }
   },
 
-  // 三方插件的选项
-  pluginOptions: {
-    // ...
+  chainWebpack: config => {
+    // less Loader
+    config.module
+      .rule("less")
+      .test(/\.less$/)
+      .use("less-loader")
+      .loader("less-loader")
+      .end()
+      .use("style-loader")
+      .loader("style-loader")
+      .end()
+      .use("css-loader")
+      .loader("css-loader")
+      .end();
+    config.module
+      .rule("md")
+      .test(/\.md/)
+      .use("vue-loader")
+      .loader("vue-loader")
+      .end()
+      .use("vue-markdown-loader")
+      .loader("vue-markdown-loader/lib/markdown-compiler")
+      .options({
+        raw: true
+      });
+  },
+
+  // CSS 相关选项
+  css: {
+    // 为预处理器的 loader 传递自定义选项。比如传递给
+    // sass-loader 时，使用 `{ sass: { ... } }`。
+    loaderOptions: {
+      scss: {}
+    }
   }
 };
